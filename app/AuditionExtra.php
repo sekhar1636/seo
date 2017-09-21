d67219c81277b5b62bd8459cfc887dba66b6df5d
Bala <bala@zaigoinfotech.com>
2017-09-21 20:24:28 +0530

    final product

---
+++ app/AuditionExtra.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AuditionExtra extends Model
+{
+    protected $table = 'audition_extra';
+
+    protected $fillable = ['user_id', 'last_audition_year','last_audition_two','last_year_year','audition_last_apply','summer_stock_last_year','where_place','unpaid_apprentice','internship','standby_appointment','emca','sag','aftra','agva','agma','friday_m','friday_af','saturday_m','saturday_af','sunday_m','sunday_af'];
+}
