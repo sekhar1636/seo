e968bd0da918bcd22f8b0366c860a4219aafbdbc
Bala <bala@zaigoinfotech.com>
2017-10-26 16:43:41 +0530

    email confirm fixed as html

--- resources/views/email/email.blade.php
+++ resources/views/email/email.blade.php
@@ -1,2 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
 <h1>Click the Link To Verify Your Email</h1>
-Click the following link to verify your email {{ url('/verifyemail/'.$email_token) }}
+<p>To verify your email <a href="{{ url('/verifyemail/'.$email_token) }}">Please Click Here!</a></p>
+</body>
+</html>
