aa23e129f420e824b42c3295ebbe8be7fa815868
balazaigo <bala@zaigoinfotech.com>
2018-08-08 13:14:14 +0530

    *Younger page editable admin side controller fix

--- app/Http/Controllers/YoungerController.php
+++ app/Http/Controllers/YoungerController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Slide;
-
+use App\StaticPage;
 
 class YoungerController extends Controller
 {
     /* Get route map to /younger */
     public function getYounger(){
         $slides = Slide::latest()->where("slideshow_id","=",2)->orderBy('id', 'desc')->get();
-        return view('common.younger',compact('slides'))->with('youngeractive','active');
+        $static = StaticPage::where('slug','younger')->get();
+        $sP = $static[0]->page_description;
+        return view('common.younger',compact('slides'))->with(['youngeractive'=>'active','sP' => $sP]);
     }
 }
